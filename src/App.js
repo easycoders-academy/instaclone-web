cd4f61f790c1fcb1178c358218424f16dc7d76a7
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-20 04:30:09 +0300

    8.3 Настройка роутера - часть 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Главная страница</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Войти в аккаунт</h1>
+          </Route>
+          <Route path="/test">
+            <h1>Проверка</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

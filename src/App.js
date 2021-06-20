8a6f241d61242fb67c5f868a381bc85800f0a9c0
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-20 23:24:43 +0300

    8.7 Знакомство с Styled Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useReactiveVar } from "@apollo/client";
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,37 @@
+import { useState } from "react";
+import styled, { css } from "styled-components";
 import { isLoggedInVar } from "../apollo";
 
+const Title = styled.h1`
+  color: ${(props) => (props.test ? "blue" : "beige")};
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  ${(props) =>
+    props.test
+      ? css`
+          font-size: 50px;
+        `
+      : css`
+          font-size: 20px;
+        `}
+`;
+
+const Container = styled.div`
+  background-color: tomato;
+`;
+
+const ToggleButton = styled.button`
+  color: red;
+`;
+
 function Login() {
+  const [test, setTest] = useState(false);
+  const toggleSetTest = () => setTest((current) => !current);
   return (
-    <div>
-      <h1>Войти в аккаунт</h1>
-      <button onClick={() => isLoggedInVar(true)}>Войти</button>
-    </div>
+    <Container>
+      <Title test={test}>Войти в аккаунт</Title>
+      <ToggleButton onClick={toggleSetTest}>Переключить!</ToggleButton>
+    </Container>
   );
 }
 export default Login;

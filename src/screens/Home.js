77313c1f4927a004a24c2277ce84677610a62d26
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-20 21:20:50 +0300

    8.5 Логика авторизации

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,9 @@
-function Home() {
-  return <h1>Главная страница</h1>;
+function Home({ setIsLoggedIn }) {
+  return (
+    <div>
+      <h1>Главная страница</h1>
+      <button onClick={() => setIsLoggedIn(false)}>Выйти из аккаунта</button>
+    </div>
+  );
 }
 export default Home;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,9 @@
-function Login() {
-  return <h1>Войти в аккаунт</h1>;
+function Login({ setIsLoggedIn }) {
+  return (
+    <div>
+      <h1>Войти в аккаунт</h1>
+      <button onClick={() => setIsLoggedIn(true)}>Войти</button>
+    </div>
+  );
 }
 export default Login;

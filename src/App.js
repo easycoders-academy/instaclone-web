5db07911398b1af019426e2c61389344e56e3570
Anton Golosnichenko <golosnichenko@artics.ru>
2021-06-21 02:59:13 +0300

    8.9 Глобальные стили в Styled Components

--- src/App.js
+++ src/App.js
@@ -5,23 +5,15 @@ import { darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
-  const lightTheme = {
-    fontColor: "#2c2c2c",
-    bgColor: "lightgray",
-  };
-
-  const darkTheme = {
-    fontColor: "lightgray",
-    bgColor: "#2c2c2c",
-  };
-
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
       <Router>
         <Switch>
           <Route path="/" exact>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,9 +5,7 @@ const Title = styled.h1`
   color: ${(props) => props.theme.fontColor};
 `;
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
+const Container = styled.div``;
 
 function Login() {
   return (

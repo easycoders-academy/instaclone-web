e164d075e3bd6073751a8028df74142a837c6562
Anton Golosnichenko <golosnichenko@artics.ru>
2021-08-06 02:39:57 +0300

    11.5 Пост в ленте - часть 2

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const SAvatar = styled.div`
-  width: 25px;
-  height: 25px;
+  width: ${(props) => (props.lg ? "35px" : "25px")};
+  height: ${(props) => (props.lg ? "35px" : "25px")};
   border-radius: 50%;
   background-color: #2c2c2c;
   overflow: hidden;
@@ -12,8 +12,8 @@ const Img = styled.img`
   max-width: 100%;
 `;
 
-function Avatar({ url = "" }) {
-  return <SAvatar>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
+function Avatar({ url = "", lg }) {
+  return <SAvatar lg={lg}>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
 }
 
 export default Avatar;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,13 @@ import styled from "styled-components";
 import Avatar from "../components/Avatar";
 import { LogoutUser } from "../apollo";
 import { BoldText } from "../components/shared";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons";
 
 const SEE_FEED_QUERY = gql`
   query seeFeed {
@@ -26,16 +33,45 @@ const PostContainer = styled.div`
   background-color: white;
   border: 1px solid ${(props) => props.theme.borderColor};
   margin-bottom: 20px;
+  max-width: 620px;
 `;
 
 const PostHeader = styled.div`
   display: flex;
   align-items: center;
-  padding: 5px 10px;
+  padding: 15px;
 `;
 
 const Username = styled(BoldText)`
-  margin-left: 10px;
+  margin-left: 15px;
+`;
+
+const PostContent = styled.img`
+  max-width: 100%;
+  min-width: 100%;
+`;
+
+const PostFooter = styled.div`
+  padding: 15px;
+`;
+
+const PostActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const PostAction = styled.div`
+  margin-right: 15px;
+`;
+
+const Likes = styled(BoldText)`
+  display: block;
+  margin-top: 15px;
 `;
 
 function Home() {
@@ -45,9 +81,33 @@ function Home() {
       {data?.seeFeed?.map((post) => (
         <PostContainer key={post.id}>
           <PostHeader>
-            <Avatar url={post?.user?.avatar} />
+            <Avatar url={post?.user?.avatar} lg={true} />
             <Username>{post?.user?.username}</Username>
           </PostHeader>
+          <PostContent src={post?.file} />
+          <PostFooter>
+            <PostActions>
+              <div>
+                <PostAction>
+                  <FontAwesomeIcon size={"lg"} icon={faHeart} />
+                </PostAction>
+                <PostAction>
+                  <FontAwesomeIcon size={"lg"} icon={faComment} />
+                </PostAction>
+                <PostAction>
+                  <FontAwesomeIcon size={"lg"} icon={faTelegramPlane} />
+                </PostAction>
+              </div>
+              <div>
+                <FontAwesomeIcon size={"lg"} icon={faBookmark} />
+              </div>
+            </PostActions>
+            <Likes>
+              {post?.likes === 1
+                ? '1 отметка "Нравится"'
+                : `${post?.likes} отметок "Нравится"`}
+            </Likes>
+          </PostFooter>
         </PostContainer>
       ))}
     </div>

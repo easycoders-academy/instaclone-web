0675f1bf7cef30506ac1b04481d1cc53ea49db2b
Anton Golosnichenko <golosnichenko@artics.ru>
2021-08-07 22:18:51 +0300

    11.13 Парсинг хештегов - часть 1

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,18 +1,38 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { BoldText } from "../shared";
+import sanitizeHtml from "sanitize-html";
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function Comment({ author, payload }) {
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[а-яА-Я]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
+
   return (
     <CommentContainer>
       <BoldText>{author}</BoldText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentContainer>
   );
 }

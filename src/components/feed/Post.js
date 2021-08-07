24d9041b2d9472ad77a9e1f39b8a5787dc02bf70
Anton Golosnichenko <golosnichenko@artics.ru>
2021-08-07 06:08:25 +0300

    11.7 Лайк фотографии

---
+++ src/components/feed/Post.js
@@ -0,0 +1,125 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { BoldText } from "../shared";
+import Avatar from "../Avatar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+} from "@fortawesome/free-regular-svg-icons";
+import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
+import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons";
+import { gql, useMutation } from "@apollo/client";
+
+const TOGGLE_LIKE_MUTATION = gql`
+  mutation toggleLike($id: Int!) {
+    toggleLike(id: $id) {
+      ok
+      error
+    }
+  }
+`;
+
+const PostContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  border-radius: 4px;
+  margin-bottom: 60px;
+  max-width: 620px;
+`;
+
+const PostHeader = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  border-bottom: 1px solid rgb(239, 239, 239);
+`;
+
+const Username = styled(BoldText)`
+  margin-left: 15px;
+`;
+
+const PostContent = styled.img`
+  max-width: 100%;
+  min-width: 100%;
+`;
+
+const PostFooter = styled.div`
+  padding: 12px 15px;
+`;
+
+const PostActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const PostAction = styled.div`
+  margin-right: 15px;
+  cursor: pointer;
+`;
+
+const Likes = styled(BoldText)`
+  display: block;
+  margin-top: 15px;
+`;
+
+function Post({ id, user, file, isLiked, likes }) {
+  const [toogleLike, { loading }] = useMutation(TOGGLE_LIKE_MUTATION, {
+    variables: { id },
+  });
+  return (
+    <PostContainer key={id}>
+      <PostHeader>
+        <Avatar url={user?.avatar} lg={true} />
+        <Username>{user?.username}</Username>
+      </PostHeader>
+      <PostContent src={file} />
+      <PostFooter>
+        <PostActions>
+          <div>
+            <PostAction onClick={toogleLike}>
+              <FontAwesomeIcon
+                style={{ color: isLiked ? "tomato" : "inherit" }}
+                icon={isLiked ? SolidHeart : faHeart}
+              />
+            </PostAction>
+            <PostAction>
+              <FontAwesomeIcon icon={faComment} />
+            </PostAction>
+            <PostAction>
+              <FontAwesomeIcon icon={faTelegramPlane} />
+            </PostAction>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+        </PostActions>
+        <Likes>
+          {likes === 1 ? '1 отметка "Нравится"' : `${likes} отметок "Нравится"`}
+        </Likes>
+      </PostFooter>
+    </PostContainer>
+  );
+}
+
+Post.propTypes = {
+  id: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }),
+  file: PropTypes.string.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+};
+
+export default Post;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
-import styled from "styled-components";
-import Avatar from "../components/Avatar";
-import { BoldText } from "../components/shared";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBookmark,
-  faComment,
-  faHeart,
-} from "@fortawesome/free-regular-svg-icons";
-import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
-import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons";
+import Post from "../components/feed/Post";
 
 const SEE_FEED_QUERY = gql`
   query seeFeed {
@@ -30,94 +20,12 @@ const SEE_FEED_QUERY = gql`
   }
 `;
 
-const PostContainer = styled.div`
-  background-color: white;
-  border: 1px solid ${(props) => props.theme.borderColor};
-  border-radius: 4px;
-  margin-bottom: 60px;
-  max-width: 620px;
-`;
-
-const PostHeader = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 15px;
-  border-bottom: 1px solid rgb(239, 239, 239);
-`;
-
-const Username = styled(BoldText)`
-  margin-left: 15px;
-`;
-
-const PostContent = styled.img`
-  max-width: 100%;
-  min-width: 100%;
-`;
-
-const PostFooter = styled.div`
-  padding: 12px 15px;
-`;
-
-const PostActions = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  div {
-    display: flex;
-    align-items: center;
-  }
-  svg {
-    font-size: 20px;
-  }
-`;
-
-const PostAction = styled.div`
-  margin-right: 15px;
-`;
-
-const Likes = styled(BoldText)`
-  display: block;
-  margin-top: 15px;
-`;
-
 function Home() {
   const { data } = useQuery(SEE_FEED_QUERY);
   return (
     <div>
       {data?.seeFeed?.map((post) => (
-        <PostContainer key={post.id}>
-          <PostHeader>
-            <Avatar url={post?.user?.avatar} lg={true} />
-            <Username>{post?.user?.username}</Username>
-          </PostHeader>
-          <PostContent src={post?.file} />
-          <PostFooter>
-            <PostActions>
-              <div>
-                <PostAction>
-                  <FontAwesomeIcon
-                    style={{ color: post?.isLiked ? "tomato" : "inherit" }}
-                    icon={post?.isLiked ? SolidHeart : faHeart}
-                  />
-                </PostAction>
-                <PostAction>
-                  <FontAwesomeIcon icon={faComment} />
-                </PostAction>
-                <PostAction>
-                  <FontAwesomeIcon icon={faTelegramPlane} />
-                </PostAction>
-              </div>
-              <div>
-                <FontAwesomeIcon icon={faBookmark} />
-              </div>
-            </PostActions>
-            <Likes>
-              {post?.likes === 1
-                ? '1 отметка "Нравится"'
-                : `${post?.likes} отметок "Нравится"`}
-            </Likes>
-          </PostFooter>
-        </PostContainer>
+        <Post key={post.id} {...post} />
       ))}
     </div>
   );

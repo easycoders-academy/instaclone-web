5e2345cd34df18310bf407ed1df69906443f684a
Anton Golosnichenko <golosnichenko@artics.ru>
2021-08-09 01:20:15 +0300

    12.3 Подписка и отписка - часть 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -60,7 +60,9 @@ function Header() {
           {isLoggedIn ? (
             <IconContainer>
               <Icon>
-                <FontAwesomeIcon icon={faHome} size="lg" />
+                <Link to={routes.home}>
+                  <FontAwesomeIcon icon={faHome} size="lg" />
+                </Link>
               </Icon>
               <Icon>
                 <FontAwesomeIcon icon={faCompass} size="lg" />
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,9 @@
 import { gql, useQuery } from "@apollo/client";
+import { faComment, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { BoldText } from "../components/shared";
 import { POST_FRAGMENT } from "../fragments";
 
 const SEE_PROFILE_QUERY = gql`
@@ -22,11 +26,133 @@ const SEE_PROFILE_QUERY = gql`
   ${POST_FRAGMENT}
 `;
 
+const Header = styled.div`
+  display: flex;
+`;
+const Avatar = styled.img`
+  margin-left: 50px;
+  height: 160px;
+  width: 160px;
+  border-radius: 50%;
+  margin-right: 150px;
+  background-color: #2c2c2c;
+`;
+const Column = styled.div``;
+const Username = styled.h3`
+  font-size: 28px;
+  font-weight: 400;
+`;
+const Row = styled.div`
+  margin-bottom: 20px;
+  font-size: 16px;
+`;
+const List = styled.ul`
+  display: flex;
+`;
+const Item = styled.li`
+  margin-right: 20px;
+`;
+const Value = styled(BoldText)`
+  font-size: 18px;
+`;
+const Name = styled(BoldText)`
+  font-size: 20px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-auto-rows: 290px;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin-top: 50px;
+`;
+
+const Photo = styled.div`
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  position: relative;
+`;
+
+const Icons = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  opacity: 0;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const Icon = styled.span`
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  margin: 0px 5px;
+  svg {
+    font-size: 14px;
+    margin-right: 5px;
+  }
+`;
+
 function Profile() {
   const { username } = useParams();
   const { data } = useQuery(SEE_PROFILE_QUERY, { variables: { username } });
   console.log(data);
-  return "Профиль пользователя";
+  return (
+    <div>
+      <Header>
+        <Avatar src={data?.seeProfile?.avatar} />
+        <Column>
+          <Row>
+            <Username>{data?.seeProfile?.username}</Username>
+          </Row>
+          <Row>
+            <List>
+              <Item>
+                <span>
+                  <Value>{data?.seeProfile?.totalFollowers}</Value> подписчиков
+                </span>
+              </Item>
+              <Item>
+                <span>
+                  <Value>{data?.seeProfile?.totalFollowing}</Value> подписок
+                </span>
+              </Item>
+            </List>
+          </Row>
+          <Row>
+            <Name>
+              {data?.seeProfile?.firstName}
+              {"  "}
+              {data?.seeProfile?.lastName}
+            </Name>
+          </Row>
+          <Row>{data?.seeProfile?.bio}</Row>
+        </Column>
+      </Header>
+      <Grid>
+        {data?.seeProfile?.photos.map((photo) => (
+          <Photo bg={photo.file}>
+            <Icons>
+              <Icon>
+                <FontAwesomeIcon icon={faHeart} />
+                {photo.likes}
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faComment} />
+                {photo.commentsNumber}
+              </Icon>
+            </Icons>
+          </Photo>
+        ))}
+      </Grid>
+    </div>
+  );
 }
 
 export default Profile;
